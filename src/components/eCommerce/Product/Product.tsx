a5e60bb2bab6e6e85048760f3bfdf1bbefb4909c
maya <maya.e@webased.net>
2024-09-15 06:27:21 -0700

    button animation

--- src/components/eCommerce/Product/Product.tsx
+++ src/components/eCommerce/Product/Product.tsx
@@ -1,14 +1,33 @@
-import { Button } from "react-bootstrap";
+import { Button, Spinner } from "react-bootstrap";
 import styles from "./styles.module.css";
 import { Tproduct } from "@customTypes/product";
 import { useDispatch } from "react-redux";
 import { addToCart } from "@store/cart/cartSlice";
+import { useEffect, useState } from "react";
 const { product, productImg } = styles;
 
 const Product = ({id , title ,img ,price }:Tproduct) => {
+ 
+ const [isBtnClicked,setIsBtnClicked] = useState(0);
+ const [isBtnDisabled, setIsBtnDisabled] = useState(false);
+ useEffect(()=>{
+   if(!isBtnClicked)
+     return;
+   setIsBtnDisabled(true)
+   const debounse = setTimeout(()=>{
+    setIsBtnDisabled(false)
+
+   },300)
+   
+   return ()=>clearTimeout(debounse);
+   
+
+ },[isBtnClicked])
+
   const dispatch = useDispatch();
   const addToCartHandler =()=>{
     dispatch (addToCart(id));
+    setIsBtnClicked((prev)=>prev+1);
 
   }
   return (
@@ -22,8 +41,11 @@ const Product = ({id , title ,img ,price }:Tproduct) => {
       <h2 title={title}>{title}</h2>
       <h3>{price}</h3>
       <Button variant="info" style={{ color: "white" }} 
-              onClick={addToCartHandler}  >
-        Add to cart
+              onClick={addToCartHandler} 
+              disabled={isBtnDisabled}
+              >
+                 {isBtnDisabled? <><Spinner animation="border" size="sm"/>Loading</> :"Add to cart" }
+        
       </Button>
     </div>
   );
